cba048b3902c4dae9857d1747934a463135fc11a
blueberry <ansisiddhu@gmail.com>
2026-01-10 17:02:08 +0530

    Added player walk animation
    Introduced Animator and animation clips for player idle and walk states, updated PlayerMovement to control animation, and connected Animator in the scene. Character sprite was split into multiple frames for animation, and the scene was updated to use the new system. Also adjusted player sprite size and slider rates.

--- Assets/Scripts/PlayerMovement.cs
+++ Assets/Scripts/PlayerMovement.cs
@@ -5,6 +5,7 @@ public class PlayerMovement : MonoBehaviour
     public float speed = 5f;
     public Rigidbody2D body;
     private Vector2 movement;
+    public Animator animator;
 
     Vector3 originalScale;
 
@@ -15,7 +16,6 @@ public class PlayerMovement : MonoBehaviour
 
     void Update()
     {
-        // WASD ONLY
         movement.x = Input.GetAxisRaw("WASD_Horizontal");
         movement.y = Input.GetAxisRaw("WASD_Vertical");
         movement.Normalize();
@@ -24,6 +24,11 @@ public class PlayerMovement : MonoBehaviour
             transform.localScale = new Vector3(-originalScale.x, originalScale.y, originalScale.z);
         else if (movement.x < 0)
             transform.localScale = originalScale;
+
+        if(movement.x != 0 || movement.y != 0)
+            animator.SetBool("isMoving", true);
+        else
+            animator.SetBool("isMoving", false);
     }
 
     void FixedUpdate()
