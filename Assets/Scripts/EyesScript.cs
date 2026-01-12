73bc3a4f2bfad596c9a83cd7db0260d82c9415ab
blueberry <ansisiddhu@gmail.com>
2026-01-12 12:13:03 +0530

    Added peek feature

--- Assets/Scripts/EyesScript.cs
+++ Assets/Scripts/EyesScript.cs
@@ -5,20 +5,28 @@ public class EyesScript : MonoBehaviour
     public GameObject leftEye;
     public GameObject rightEye;
 
-    public GameObject blockWhileExists;   // ← drag your cutscene controller here
+    public GameObject blockWhileExists;
 
     bool leftClosed = false;
     bool rightClosed = false;
+
+    SpriteRenderer leftSR;
+    SpriteRenderer rightSR;
+    public GameObject peekObject;
+
 
     void Start()
     {
         leftEye.SetActive(false);
         rightEye.SetActive(false);
+
+        leftSR = leftEye.GetComponent<SpriteRenderer>();
+        rightSR = rightEye.GetComponent<SpriteRenderer>();
     }
 
     void Update()
     {
-        if (blockWhileExists != null)
+        if (blockWhileExists && blockWhileExists.activeInHierarchy)
             return;
 
         if (Input.GetKeyDown(KeyCode.C))
@@ -31,6 +39,50 @@ public class EyesScript : MonoBehaviour
         {
             rightClosed = !rightClosed;
             rightEye.SetActive(rightClosed);
+        }
+
+        Peek();
+    }
+
+
+    void Peek()
+    {
+        // Only allow peeking when BOTH eyes are closed
+        if (!leftClosed || !rightClosed)
+        {
+            SetEyeOpacity(1f);
+            if (peekObject) peekObject.SetActive(false);
+            return;
+        }
+
+        // Both eyes closed = show peek object
+        if (peekObject) peekObject.SetActive(true);
+
+        if (Input.GetKey(KeyCode.P))
+        {
+            SetEyeOpacity(0.7f);
+        }
+        else
+        {
+            SetEyeOpacity(1f);
+        }
+    }
+
+
+    void SetEyeOpacity(float alpha)
+    {
+        if (leftSR)
+        {
+            Color c = leftSR.color;
+            c.a = alpha;
+            leftSR.color = c;
+        }
+
+        if (rightSR)
+        {
+            Color c = rightSR.color;
+            c.a = alpha;
+            rightSR.color = c;
         }
     }
 }
